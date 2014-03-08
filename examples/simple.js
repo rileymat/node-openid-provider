63636f879ea3dacc0d0854fac5ec522961e21ad5
Chris Reid <chris@reidsy.com>
2014-03-08 16:24:15 +1100

    Changed indentation to use spaces as per airbnb javascript style guide

--- examples/simple.js
+++ examples/simple.js
@@ -1,10 +1,10 @@
 var HOSTNAME = 'localhost';
 var PORT = 3000;
 function OPENID_ENDPOINT() {
-	return "http://"+HOSTNAME+":"+PORT+"/";
+  return "http://"+HOSTNAME+":"+PORT+"/";
 }
 function OPENID_USER_ENDPOINT(username) {
-	return "http://"+HOSTNAME+":"+PORT+"/"+username.toLowerCase();
+  return "http://"+HOSTNAME+":"+PORT+"/"+username.toLowerCase();
 }
 
 var express = require('express');
@@ -22,31 +22,31 @@ app.use(oidp.middleware());
 
 //create an XRDS Document for the server
 app.get('/', function(req, res, next) {
-	res.header('Content-Type', "application/xrds+xml");
-	res.send(oidp.XRDSDocument());
-	res.end();
+  res.header('Content-Type', "application/xrds+xml");
+  res.send(oidp.XRDSDocument());
+  res.end();
 });
 
 //handle authentication
 app.post('/', function(req, res, next) {
-	if(req.oidp) {
-		//here you would perform the login procedure, such as display a login page
-		//assuming the user 'Chris' is already logged in, complete the authentication
-		var url = oidp.checkid_setup_complete(req.oidp, OPENID_USER_ENDPOINT('Chris'));
-		res.redirect(303, url);
-		res.end();
-	}
-	else {
-		res.send(400, 'Not an OpenID Request');
-		res.end();
-	}
+  if(req.oidp) {
+    //here you would perform the login procedure, such as display a login page
+    //assuming the user 'Chris' is already logged in, complete the authentication
+    var url = oidp.checkid_setup_complete(req.oidp, OPENID_USER_ENDPOINT('Chris'));
+    res.redirect(303, url);
+    res.end();
+  }
+  else {
+    res.send(400, 'Not an OpenID Request');
+    res.end();
+  }
 });
 
 //create an XRDS Document for a user
 app.get('/:username', function(req, res, next) {
-	res.header('Content-Type', "application/xrds+xml");
-	res.send(oidp.XRDSDocument(OPENID_USER_ENDPOINT(req.params.username)));
-	res.end();
+  res.header('Content-Type', "application/xrds+xml");
+  res.send(oidp.XRDSDocument(OPENID_USER_ENDPOINT(req.params.username)));
+  res.end();
 });
 
 app.listen(PORT);
--- test/utils.js
+++ test/utils.js
@@ -3,36 +3,36 @@ var utils = require('../lib/Utils.js');
 
 suite("Utilities Tests", function() {
 
-    test("Test addOpenIDNamespace", function() {
-        var incoming = {
-            first: "one",
-            second: "two",
-            third: "three"
-        };
-        var outgoing = {
-            "openid.first": "one",
-            "openid.second": "two",
-            "openid.third": "three"
-        };
-        var result = utils.addOpenIDNamespace(incoming);
-        assert.deepEqual(result, outgoing);
-    });
+  test("Test addOpenIDNamespace", function() {
+    var incoming = {
+      first: "one",
+      second: "two",
+      third: "three"
+    };
+    var outgoing = {
+      "openid.first": "one",
+      "openid.second": "two",
+      "openid.third": "three"
+    };
+    var result = utils.addOpenIDNamespace(incoming);
+    assert.deepEqual(result, outgoing);
+  });
 
-    test("Test getOpenIDFields", function() {
-        var incoming = {
-            "openid.first": "one",
-            "openid.second": "two",
-            "openid.third": "three",
-            "fourth": "four",
-            "fifth": "five"
-        };
-        var outgoing = {
-            "first": "one",
-            "second": "two",
-            "third": "three"
-        };
-        var result = utils.getOpenIDFields(incoming);
-        assert.deepEqual(result, outgoing);
-    });
+  test("Test getOpenIDFields", function() {
+    var incoming = {
+      "openid.first": "one",
+      "openid.second": "two",
+      "openid.third": "three",
+      "fourth": "four",
+      "fifth": "five"
+    };
+    var outgoing = {
+      "first": "one",
+      "second": "two",
+      "third": "three"
+    };
+    var result = utils.getOpenIDFields(incoming);
+    assert.deepEqual(result, outgoing);
+  });
 
 });
